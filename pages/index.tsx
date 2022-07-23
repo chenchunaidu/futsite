2b0118abd68a8c8721a0f2325276c59ffb5cc9c2
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-07-23 22:46:45 +0530

    fix: added home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { Text, Title, Center, Group, Button } from "@mantine/core";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <Center sx={{ height: "95vh" }}>
+      <Group direction="column" align="center">
+        <Title sx={{ fontSize: "144px" }}>Welcome to futsite</Title>
+        <Text sx={{ fontSize: "36px", color: "gray" }}>
+          Convert your design into code of any UI language
+        </Text>
+        <Button>
+          <Link href="/editor"> Go to editor </Link>
+        </Button>
+      </Group>
+    </Center>
+  );
 };
 
 export default Home;
